d3baeb11d1e7650281b6354279c8f5dbe880f766
NV4RE <tosbodesz@gmail.com>
2019-10-02 19:30:37 +0700

    Fix cannot import

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
-export * from './event';
-export * from './kafka';
-export * from './state';
-export * from './task';
-export * from './taskDefinition';
-export * from './transaction';
-export * from './workflow';
-export * from './workflowDefinition';
+import * as Event from './event';
+import * as Kafka from './kafka';
+import * as State from './state';
+import * as Task from './task';
+import * as TaskDefinition from './taskDefinition';
+import * as Transaction from './transaction';
+import * as Workflow from './workflow';
+import * as WorkflowDefinition from './workflowDefinition';
+
+export {
+  Event,
+  Kafka,
+  State,
+  Task,
+  TaskDefinition,
+  Transaction,
+  Workflow,
+  WorkflowDefinition,
+};
