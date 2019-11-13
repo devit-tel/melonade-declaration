6a4ceaf13626d732dc48f1e67a6d661847a70b4f
NV4RE <tosbodesz@gmail.com>
2019-11-13 12:48:24 +0700

    Remove all sub-workflow

--- src/workflow.ts
+++ src/workflow.ts
@@ -3,7 +3,6 @@ import { IWorkflowDefinition } from './workflowDefinition';
 
 export enum WorkflowTypes {
   Workflow = 'WORKFLOW', // When Completed make transaction COMPLETED
-  SubWorkflow = 'SUB_WORKFLOW', // When Completed make parent task COMPLETED
   CompensateWorkflow = 'COMPENSATE_WORKFLOW', // When Completed Make Transaction CANCELLED
   CompensateThenRetryWorkflow = 'COMPENSATE_THEN_WORKFLOW', // When Completed Start original workflow
   CancelWorkflow = 'CANCEL_WORKFLOW',
