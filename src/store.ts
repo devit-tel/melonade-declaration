4d9492b1be4868f369b09d16d76c1cebc94d4663
NV4RE <tosbodesz@gmail.com>
2019-12-30 19:35:07 +0700

    Add Interfaces for UI

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,23 @@
+import { ITransactionEvent } from './event';
+
 export enum StoreType {
   ZooKeeper = 'ZOOKEEPER', // Greate for Definition
   MongoDB = 'MONGODB',
   Redis = 'REDIS', // Greate for Instance
   Memory = 'MEMORY', // For Dev/Test, don't use in production
+}
+
+export interface IHistogramCount {
+  date: number | Date;
+  count: number;
+}
+
+export interface ITaskExecutionTime {
+  executionTime: number;
+  taskName: string;
+}
+
+export interface ITransactionEventPaginate {
+  total: number;
+  events: ITransactionEvent[];
 }
